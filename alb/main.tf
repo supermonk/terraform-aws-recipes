736a1ead84ff6b9afc15eb40f3c8cb0d9e82c657
mattb <mattb@pageuppeople.com>
2017-01-18 15:07:51 +1100

    Alarm

--- alb/main.tf
+++ alb/main.tf
@@ -22,13 +22,34 @@ resource "aws_alb_target_group" "default" {
 
 resource "aws_alb_listener" "default_https" {
   load_balancer_arn = "${aws_alb.default.id}"
-  port = "443"
-  protocol = "HTTPS"
-  ssl_policy = "ELBSecurityPolicy-2015-05"
-  certificate_arn = "${var.alb_ssl_certificate_id}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "${var.alb_ssl_certificate_id}"
 
   default_action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = "${aws_alb_target_group.default.id}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "health-check" {
+  alarm_name          = "${var.alb_name}-health-check"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/ApplicationELB"
+  period              = "300"
+  statistic           = "Minimum"
+  threshold           = "1"
+
+  alarm_description         = "[[Standard format defined by Anton]]"
+  ok_actions                = ["${var.eagle_eye_dashboard}", "arn:aws:sns:ap-southeast-2:047651431481:Mattb"]
+  insufficient_data_actions = ["${var.eagle_eye_dashboard}", "arn:aws:sns:ap-southeast-2:047651431481:Mattb"]
+  alarm_actions             = ["${var.eagle_eye_dashboard}", "arn:aws:sns:ap-southeast-2:047651431481:Mattb"]
+
+  dimensions {
+    LoadBalancer = "${aws_alb.default.name}"
+    TargetGroup  = "${aws_alb_target_group.default.name}"
   }
 }
--- alb/variables.tf
+++ alb/variables.tf
@@ -6,20 +6,21 @@ variable "vpc_id" {
   description = "The VPC"
 }
 
-variable "alb_name" {
-}
+variable "alb_name" {}
 
 variable "alb_is_internal" {
-  default = "false"
+  default     = "false"
   description = "internal or external alb for the web role, default is false."
 }
 
 variable "alb_subnets" {
-  description = "Vpc subnets where the load balance for web service should be launched"  
+  description = "Vpc subnets where the load balance for web service should be launched"
 }
 
-variable "alb_ssl_certificate_id" {
-}
+variable "alb_ssl_certificate_id" {}
 
-variable "stream_tag" {
+variable "stream_tag" {}
+
+variable "eagle_eye_dashboard" {
+  default = "arn:aws:sns:ap-southeast-2:047651431481:EagleEye-Development-Alerts"
 }
